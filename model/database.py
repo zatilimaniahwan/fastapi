377b71483c1b37252894679f239a5aa5de380279
Zatil Imani <zatilimaniahwan@gmail.com>
2021-07-21 00:42:22 +0800

    Update..

---
+++ model/database.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+#Config DB
+username = 'root'
+password = ''
+server = 'localhost'
+dbname = 'fastapi'
+dbType = 'mysql'
+
+SQLALCHEMY_DATABSE_URL="mysql+mysqlconnector://{username}:{password}@{server}/{dbname}".format(username, password, server,dbname) if dbType == 'mysql' else "postgresql://{username}:{password}@{server}/{dbname}".format(username, password, server,dbname)
+
+engine = create_engine (SQLALCHEMY_DATABSE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
